1df8eeebb951cba6aa21e8f65ae5336626983ddc
CHIRANJEEVICHETAN <chiranjeevichetan1996@gmail.com>
2026-04-01 15:46:43 +0530

    feat(analytics): integrate Vercel analytics and speed insights components
    - Added @vercel/analytics and @vercel/speed-insights dependencies to package.json.
    - Updated main.tsx to include Analytics and SpeedInsights components for enhanced performance tracking and insights.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './index.css'
 import App from './App.tsx'
 import { APIProvider } from './lib/api/provider.tsx'
@@ -16,6 +18,8 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <APIProvider>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </APIProvider>
     </BrowserRouter>
   </StrictMode>,
